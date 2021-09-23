cd66e136cb7170c78a676132d8f5e28a85e3c527
aleks <aleks.mihaylov69@gmail.com>
2021-09-23 10:25:43 +0300

    add dynamic elements

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,17 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const cards = document.querySelectorAll('.card');
+  console.log(cards);
+  
+  setTimeout(() => {
+    cards.forEach(card => {
+      const str = card.classList.toString()
+      console.log(str)
+      if(!str.includes('active')){
+        card.style.display = 'none';
+      }
+    })
+  }, 3000)
+
 });
